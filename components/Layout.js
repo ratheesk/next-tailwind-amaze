0818fb97826bbd5dd006e7f24efbe5c423cfce54
Rathees-Koneswarann <ratheesreo@gmail.com>
2023-03-05 13:49:33 +0530

    feat: add layout component

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' | Amaze' : 'Amaze'}</title>
+        <meta name="description" content="Ecommerce Application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link href="/">
+              <span className="text-lg font-bold">amaze</span>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <span className="p-2">Cart</span>
+              </Link>
+              <Link href="/login">
+                <span className="p-2">Login</span>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Copy right @2023 Amaze
+        </footer>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Ecommerce Application" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <p className="text-3xl underline">heading</p>
-    </div>
-  );
+  return <Layout title="Home">Home page</Layout>;
 }
